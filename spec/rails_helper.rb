688c04c166f327e21abdec2f0fa71ba017473278
anastasiya <maxtsoy@meta.ua>
2021-04-06 23:00:00 +0300

    fix change rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,18 +13,11 @@ require 'devise'
 require 'faker'
 require 'cancan/matchers'
 
-Dir["#{File.dirname(__FILE__)}/support/*.rb"].sort.each { |file| require file }
-
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |file| require file }
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  # config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 end
