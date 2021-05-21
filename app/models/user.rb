482076ef19d666ac150bd31a2c37eb8fa5762831
anastasiya <maxtsoy@meta.ua>
2021-05-21 22:26:10 +0300

    add model User

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  PASSWORD_FORMAT = /\A(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])/x
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable,
+         :omniauthable, omniauth_providers: %i[facebook]
+  
+  has_many :addresses, as: :addressable, dependent: :destroy
+
+  validate :password_regex
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
+
+  private
+
+  def password_regex
+    if password.present?
+       if !password.match(PASSWORD_FORMAT) 
+         errors.add :password, I18n.t('message.error.user.wrong_format')
+       end
+    end
+  end
+end
