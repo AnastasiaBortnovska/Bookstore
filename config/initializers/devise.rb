3654e33817762128823b4019ac07c8799fc7492b
anastasiya <maxtsoy@meta.ua>
2021-05-24 16:07:41 +0300

    fix omniauth authorize

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,8 @@
 Devise.setup do |config|
 
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
+  
+  require 'omniauth-facebook'
   require 'devise/orm/active_record'
 
   config.case_insensitive_keys = [:email]
