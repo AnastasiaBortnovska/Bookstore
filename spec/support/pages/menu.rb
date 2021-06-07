5d6fb706683eff224ee857f5e203807efe9e01f2
anastasiya <maxtsoy@meta.ua>
2021-06-07 23:54:18 +0300

    fix spec

--- spec/support/pages/menu.rb
+++ spec/support/pages/menu.rb
@@ -2,4 +2,6 @@
 
 class Menu < SitePrism::Section
   element :item_menu, 'a'
+  element :button_log_in, 'a', text: I18n.t('partials.header.log_in')
+  element :button_sing_up, 'a', text: I18n.t('partials.header.sign_up')
 end
