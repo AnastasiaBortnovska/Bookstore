ce02ed07f2317c54e75c42ff400ed5ff6423e387
AnastasiaBortnovska <anastasiia.bortnovska@rubygarage.org>
2021-11-29 13:57:16 +0200

    fix

--- config/puma.rb
+++ config/puma.rb
@@ -7,7 +7,7 @@ threads min_threads_count, max_threads_count
 
 worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 
-port ENV.fetch('PORT', 3000)
+port ENV.fetch('PORT_PUMA', 3000)
 environment ENV.fetch('RAILS_ENV', 'development')
 pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 plugin :tmp_restart
