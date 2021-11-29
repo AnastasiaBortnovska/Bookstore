50b5b2c78fc9a471144e0088e3f83147a9687ce2
AnastasiaBortnovska <anastasiia.bortnovska@rubygarage.org>
2021-11-29 14:33:14 +0200

    fix

--- config/puma.rb
+++ config/puma.rb
@@ -4,13 +4,12 @@ require 'fileutils'
 max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
 min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 threads min_threads_count, max_threads_count
+preload_app!
 
 worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 
 port ENV.fetch('PORT_PUMA', 3000)
 environment ENV.fetch('RAILS_ENV', 'development')
 pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 plugin :tmp_restart
-before_fork do |server,worker|
-	FileUtils.touch('./tmp/app-initialized')
-end
